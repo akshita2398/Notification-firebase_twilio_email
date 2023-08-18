209bb647db384387979d3947d4ff2c8ebe5bfabd
akshita_appinventiv <akshita.manglik@appinventiv.com>
2023-08-18 19:00:37 +0530

    fillable

--- src/Models/UserDeviceToken.php
+++ src/Models/UserDeviceToken.php
@@ -4,8 +4,13 @@ namespace Akshita\NotificationFirebaseTwilioEmailPackage\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserNotLoginDeviceToken extends Model
+class UserDeviceToken extends Model
 {    
+    protected $fillable = [
+        'device_token',
+        'device_type'
+    ];
+    
     public function users()
     {
         return $this->belongsTo(Users::class);
--- src/Models/UserNotLoginDeviceToken.php
+++ src/Models/UserNotLoginDeviceToken.php
@@ -4,8 +4,14 @@ namespace Akshita\NotificationFirebaseTwilioEmailPackage\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserDeviceToken extends Model
+class UserNotLoginDeviceToken extends Model
 {    
+    protected $fillable = [
+        'user_id',
+        'device_token',
+        'device_type'
+    ];
+
     public function users()
     {
         return $this->belongsTo(Users::class);
