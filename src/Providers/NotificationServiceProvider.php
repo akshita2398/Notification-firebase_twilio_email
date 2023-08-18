785c3beda0f7e6f8185d8568154c966926142974
akshita_appinventiv <akshita.manglik@appinventiv.com>
2023-08-18 18:10:46 +0530

    add migration

--- src/Providers/NotificationServiceProvider.php
+++ src/Providers/NotificationServiceProvider.php
@@ -7,17 +7,18 @@ use Akshita\NotificationFirebaseTwilioEmailPackage\Notification;
 
 class NotificationServiceProvider extends ServiceProvider
 {
-    
+
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/notification.php' => config_path('notification.php'),
+            __DIR__ . '/notification.php' => config_path('notification.php'),
         ], 'notification-config');
-    
-        $this->mergeConfigFrom(__DIR__.'/notification.php', 'notification');
-    
-        $this->mergeConfigFrom(__DIR__.'/notificationContent.php', 'notificationContent');
-        
+
+        $this->mergeConfigFrom(__DIR__ . '/notification.php', 'notification');
+
+        $this->mergeConfigFrom(__DIR__ . '/notificationContent.php', 'notificationContent');
+
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 
     public function register()
